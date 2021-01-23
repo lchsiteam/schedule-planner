8dd0a277f640599247a481885737862bfc5daa31
MC-Atom <thepnkprkr@gmail.com>
2021-01-22 16:13:21 -0800

    made things work

--- js/calculations.js
+++ js/calculations.js
@@ -2,3 +2,12 @@ var selectedClasses;
 
 selectedClasses = localStorage.getItem("selectedClasses");
 
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    classes = JSON.parse(this.responseText);
+  }
+};
+xmlhttp.open("GET", "documents/defaultClasses.json", true);
+xmlhttp.send();
+
