1c11ede7e222c1c74aa7388d6bae889c72bd762b
MC-Atom <thepnkprkr@gmail.com>
2021-01-22 16:22:56 -0800

    a

--- js/calculations.js
+++ js/calculations.js
@@ -2,6 +2,8 @@ var selectedClasses;
 
 selectedClasses = localStorage.getItem("selectedClasses");
 
+var classes;
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
